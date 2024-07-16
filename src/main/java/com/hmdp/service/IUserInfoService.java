a443f5574d82f265da2818299b083e3d3ee3067a
guojunzhang <109278202+xihuazgj@users.noreply.github.com>
2024-07-16 19:18:07 +0800

    Initial commit

---
+++ src/main/java/com/hmdp/service/IUserInfoService.java
@@ -0,0 +1,21 @@
+package com.hmdp.service;
+
+
+import com.hmdp.entity.UserInfo;
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-24
+ */
+
+public interface IUserInfoService extends IService<UserInfo> {
+
+
+}
